03a99d7fc65ca359615b0a1c882edc21f840dbde
Pulido V. P. E <pulidovpe.dev@gmail.com>
2019-05-02 10:11:17 -0400

    feat: API Rest completado
    Se completo el API usando async-await
    y try-catch para el control de errores.
    Tambien se eliminaron mensajes de mongo
    deprecated.
    
    Resolves: #0001

--- src/database.js
+++ src/database.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const URI = 'mongodb://localhost/mern-tasks';
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+
 mongoose.connect(URI)
    .then(db => console.log(`DB is connected`))
    .catch(err => console.error(err.stack));
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,10 +1,69 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res) => {
-   res.json({
-      status: 'API Works!'
-   });
+const Task = require('../models/task');
+
+router.get('/', async (req, res) => {
+   try {
+      const tasks = await Task.find();
+      res.status(200).json(tasks);
+   } catch (err) {
+      console.log(`err: ${err.status} - name: ${err.name} - message: ${err.message}`);
+      if (err.status == '503' || err.status == '504') return res.status(503).json({ error: err.message });
+      return res.status(404).json({ error: err.message });
+   }
+});
+
+router.get('/:id', async (req, res) => {
+   try {
+      const _id = req.params.id;
+      const task = await Task.findOne({ _id });
+      res.status(200).json(task);
+   } catch (err) {
+      console.log(`err: ${err.status} - name: ${err.name} - message: ${err.message}`);
+      if (err.status == '503' || err.status == '504') return res.status(503).json({ error: err.message });
+      return res.status(404).json({ error: err.message });
+   }
+});
+
+router.post('/', async (req, res) => {
+   try {
+      const { title, description } = req.body;
+      const newTask = new Task({ title, description});
+      const saved = await newTask.save();
+      res.status(201).json(saved);
+   } catch (err) {
+      console.log(`err: ${err.status} - name: ${err.name} - message: ${err.message}`);
+      if (err.name == 'ValidationError') return res.status(400).json({ error: err.message });
+      if (err.name == 'MongoError') return res.status(409).send({ error: 'Duplicate key error' });
+      return res.status(404).json({ error: err.message });
+   }
+});
+
+router.put('/:id', async (req, res) => {
+   try {
+      const _id = req.params.id;
+      const { title, description } = req.body;
+      const updTask = { title, description };
+      const updated = await Task.findByIdAndUpdate(_id, updTask);
+      res.status(200).json(updated);
+   } catch (err) {
+      console.log(`err: ${err.status} - name: ${err.name} - message: ${err.message}`);
+      if (err.name == 'ValidationError') return res.status(400).json({ error: err.message });
+      if (err.name == 'MongoError') return res.status(409).send({ error: 'Duplicate key error' });
+      return res.status(404).json({ error: err.message });
+   }
+});
+
+router.delete('/:id', async (req, res) => {
+   try {
+      const id = req.params.id;
+      await Task.findByIdAndRemove(id);
+      res.status(200).send({ status: 'Task deleted' });
+   } catch (err) {
+      console.log(`err: ${err.status} - name: ${err.name} - message: ${err.message}`);
+      return res.status(404).json({ error: err.message });
+   }
 });
 
 module.exports = router;
